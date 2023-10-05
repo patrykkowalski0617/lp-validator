1220c1cf3ad9cf7b4c0b6521317afc8739f2d0b4
Patrycjusz Kowalski <patrykkowalski0617@gmail.com>
2023-10-05 14:40:14 +0200

    refactor

--- src/script/jira/dataPickers/steps/_helpers/index.js
+++ src/script/jira/dataPickers/steps/_helpers/index.js
@@ -1,1 +1,2 @@
 export { default as steps_dataFinder } from "./steps_dataFinder";
+export { default as steps_dataPicker } from "./steps_dataPicker";
--- src/script/jira/dataPickers/steps/steps.js
+++ src/script/jira/dataPickers/steps/steps.js
@@ -1,18 +1,6 @@
-import { steps_dataFinder } from "./_helpers";
+import { steps_dataPicker } from "./_helpers";
 
 const steps = (taskContentContainers) => {
-  const steps_dataPicker = (taskContentContainers) => {
-    const stepsEl = steps_dataFinder(taskContentContainers);
-    if (!stepsEl) return;
-    const proof = stepsEl.textContent;
-    const data = proof
-      .replace(/(\r\n|\n|\r)/gm, "")
-      .replace(/\s+/g, " ")
-      .split(/[0-9]*\. /)
-      .filter((step) => step.length > 20)
-      .map((step) => step.trim());
-    return { data, proof };
-  };
   return steps_dataPicker(taskContentContainers);
 };
 
